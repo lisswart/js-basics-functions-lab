380a0c0263d776d97ec72d681686022ae36ec999
lisswart <arnoudlisa@gmail.com>
2021-03-30 15:56:35 -0400

    commit tree d33954b8d5c8aa64db38ef582b1fb6554fec7af1

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickUpLoc) {
+    return Math.abs(pickUpLoc - 42);
+}
+
+function distanceFromHqInFeet(pickUpLoc) {
+    return distanceFromHqInBlocks(pickUpLoc) * 264;
+}
+
+function distanceTravelledInFeet(begBlock, endBlock) {
+    return 264 * Math.abs(endBlock - begBlock);
+}
+
+function calculatesFarePrice(begBlock, endBlock) {
+    const distTravldInFt = distanceTravelledInFeet(begBlock, endBlock);
+
+    if(distTravldInFt <= 400) {
+        return 0;
+    }
+    else if(distTravldInFt > 400 && distTravldInFt <= 2000) {
+        return ((distTravldInFt - 400) * 2) / 100;
+    }
+    else if(distTravldInFt > 2000 && distTravldInFt <= 2500) {
+        return ((distTravldInFt - 400) * 2) / 100 + 25;
+    }
+}
